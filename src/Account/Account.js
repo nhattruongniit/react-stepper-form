369a1cbfc5ed6ff6c5ea88f61cd035c80daf592a
TonyNguyen <truong.nn@amanotes.com>
2021-01-13 13:37:58 +0700

    update react select

--- src/Account/Account.js
+++ src/Account/Account.js
@@ -1,11 +1,43 @@
 import React from 'react';
 import * as Yup from 'yup';
 import { Formik, Field, Form, FieldArray } from 'formik';
+import Select from "react-select";
 
 // material core
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 
+const options = [
+  { value: "Abe", label: "Abe", customAbbreviation: "A" },
+  { value: "John", label: "John", customAbbreviation: "J" },
+  { value: "Dustin", label: "Dustin", customAbbreviation: "D" }
+];
+
+
+const Option = (props) => {
+  console.log("props", props);
+  const { innerProps, innerRef, data } = props;
+  return (
+    <article ref={innerRef} {...innerProps}>
+      <h4>{data.label}</h4> component
+      <h5>Genre: {data.value}</h5>
+      <h6>
+        {data.customAbbreviation}
+      </h6>
+    </article>
+  );
+};
+
+const formatOptionLabel = ({ value, label, customAbbreviation }) => (
+  <div style={{ display: "flex" }}>
+    <div>{label}</div>
+    truong
+    <br/>
+    <div style={{ marginLeft: "10px", color: "#ccc" }}>
+      {customAbbreviation}
+    </div>
+  </div>
+);
 
 function Account() {
   const initialValues = {
@@ -29,6 +61,13 @@ function Account() {
   return (
     <>
       <b>Friends FieldArray</b>
+      <Select
+        defaultValue={options[0]}
+        formatOptionLabel={formatOptionLabel}
+        components={{ Option }}
+        options={options}
+      />
+      <br/>
       <Formik
         initialValues={initialValues}
         validationSchema={validationSchema}
