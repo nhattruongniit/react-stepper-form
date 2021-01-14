a5c10b91cba5d968a0f7ae8a9a34b4c52509d8bd
TonyNguyen <truong.nn@amanotes.com>
2021-01-14 11:50:54 +0700

    autozome

--- src/Account/AutoCompleteScroll.js
+++ src/Account/AutoCompleteScroll.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import parse from 'autosuggest-highlight/parse';
+import ListSubheader from '@material-ui/core/ListSubheader';
 import match from 'autosuggest-highlight/match';
+import { VariableSizeList as List } from 'react-window';
 
 import TextField from '@material-ui/core/TextField';
-const numbs = [];
+const locales = [];
 for (let i = 0; i < 500; i = i + 1) {
-  numbs.push({ 
+  locales.push({ 
     createdAt: "2020-11-04 17:30:43",
     createdBy: null,
     description: null,
@@ -19,13 +21,82 @@ for (let i = 0; i < 500; i = i + 1) {
   });
 }
 
+const renderRow = (props) => {
+  const { style, index, data } = props;
+  console.log('renderRow', props)
+  return (
+    <div style={style}>{data[index]}</div>
+  )
+}
+
+const OuterElementContext = React.createContext({});
+
+const OuterElementType = React.forwardRef((props, ref) => {
+  const outerProps = React.useContext(OuterElementContext);
+  return <div ref={ref} {...props} {...outerProps} />;
+});
+
+function useResetCache(data) {
+  const ref = React.useRef(null);
+  React.useEffect(() => {
+    if (ref.current != null) {
+      ref.current.resetAfterIndex(0, true);
+    }
+  }, [data]);
+  return ref;
+}
+
+const ListboxComponent = React.forwardRef(function ListboxComponent(props, ref) {
+  const { children, ...other } = props;
+  const itemData = React.Children.toArray(children);
+  const itemSize = 48;
+  const itemCount = itemData.length;
+  const LISTBOX_PADDING = 8;
+
+  const getChildSize = (child) => {
+    if (React.isValidElement(child) && child.type === ListSubheader) {
+      return 48;
+    }
+    return itemSize;
+  };
+
+  const getHeight = () => {
+    if (itemCount > 8) {
+      return 8 * itemSize;
+    }
+    return itemData.map(getChildSize).reduce((a, b) => a + b, 0);
+  };
+
+  const gridRef = useResetCache(itemCount);
+
+  return (
+    <div ref={ref}>
+      <OuterElementContext.Provider value={other}>
+        <List
+          itemData={itemData}
+          height={getHeight() + 2 * LISTBOX_PADDING}
+          width="100%"
+          innerElementType="div"
+          ref={gridRef}
+          outerElementType={OuterElementType}
+          itemSize={(index) => getChildSize(itemData[index])}
+          overscanCount={5}
+          itemCount={itemData.length}
+        >
+          {renderRow}
+        </List>
+      </OuterElementContext.Provider>
+    </div>
+  )
+});
+
 function AutoCompleteScroll() {
   return (
     <Autocomplete
       id="combo-box-demo"
-      options={numbs}
+      options={locales}
       getOptionLabel={(option) => option.language_region}
-      style={{ width: 300 }}
+      ListboxComponent={ListboxComponent}
       renderOption={(option, { inputValue }) => {
         const matches = match(option.language_region, inputValue);
         const parts = parse(option.language_region, matches);
